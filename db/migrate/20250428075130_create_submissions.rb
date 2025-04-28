4074eda8fc0a30f75ed0ab00ab9e7a41af63613c
Keishi Asai <isheik808@gmail.com>
2025-04-28 00:59:51 -0700

    update constraints for models

--- db/migrate/20250428075130_create_submissions.rb
+++ db/migrate/20250428075130_create_submissions.rb
@@ -1,11 +1,13 @@
 class CreateSubmissions < ActiveRecord::Migration[7.2]
   def change
     create_table :submissions do |t|
-      t.string :uuid
+      t.string :uuid, null: false
       t.text :comment
       t.string :generated_image_path
 
       t.timestamps
     end
+
+    add_index :submissions, :uuid, unique: true
   end
 end
--- db/migrate/20250428075131_create_submission_books.rb
+++ db/migrate/20250428075131_create_submission_books.rb
@@ -2,12 +2,21 @@ class CreateSubmissionBooks < ActiveRecord::Migration[7.2]
   def change
     create_table :submission_books do |t|
       t.references :submission, null: false, foreign_key: true
-      t.string :title
-      t.string :author
-      t.string :cover_url
-      t.integer :book_order
+      t.string :title, null: false
+      t.string :author, null: false
+      t.string :cover_url, null: false
+      t.integer :book_order, null: false
 
       t.timestamps
     end
+
+    add_index :submission_books, [:submission_id, :book_order], unique: true
+    
+    # book_orderは1から3までの値のみ許可
+    execute <<-SQL
+      ALTER TABLE submission_books
+      ADD CONSTRAINT check_book_order_range
+      CHECK (book_order BETWEEN 1 AND 3)
+    SQL
   end
 end
