ff2d46fc4c5eb59c4f0ba6cb19497201256ef4e7
Keishi Asai <isheik808@gmail.com>
2025-04-29 00:31:48 -0700

    update models

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,2 +1,19 @@
 class Submission < ApplicationRecord
+  has_many :submission_books, dependent: :destroy
+  
+  validates :uuid, presence: true, uniqueness: true
+  validate :must_have_three_books
+  
+  before_validation :generate_uuid, on: :create
+  
+  private
+  
+  def generate_uuid
+    self.uuid = SecureRandom.uuid
+  end
+  
+  def must_have_three_books
+    return if submission_books.size == 3
+    errors.add(:base, "投稿には3冊の本が必要です")
+  end
 end
--- app/models/submission_book.rb
+++ app/models/submission_book.rb
@@ -1,3 +1,12 @@
 class SubmissionBook < ApplicationRecord
   belongs_to :submission
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :cover_url, presence: true
+  validates :book_order, presence: true,
+                        numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 3 },
+                        uniqueness: { scope: :submission_id, message: "同じ順番の本は登録できません" }
+
+  default_scope { order(:book_order) }
 end
